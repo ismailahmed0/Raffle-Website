c140018f6e340d6be64095548bda4a70999d49f3
CSA-IAA <32169836+CSA-IAA@users.noreply.github.com>
2018-11-30 14:49:46 -0600

    Public updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9w5q^rh$u+)xay*4(w(3*^8vjy-qjufyg23mw_8g5#oi$u_*%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
